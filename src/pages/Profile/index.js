bc8b6909b5b907f01f7b296d862cf9455be4ca3a
Alisson Zotarelli <alzo.zotarelli@gmail.com>
2019-12-13 09:59:10 -0300

    Commit test

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,5 +3,12 @@ import React from 'react';
 // import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  return (
+    <>
+      <h1>Profile</h1>
+      <p>nome</p>
+      <p>email</p>
+      <input />
+    </>
+  );
 }
